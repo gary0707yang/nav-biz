13ca3ca12639bad29c01c306c1e8a612dcdf2768
gary <feiyang@live.cn>
2022-02-08 18:19:18 +0800

    update

---
+++ update.js
@@ -0,0 +1,29 @@
+// 1. 公众号二维码
+const wechat_qr =
+
+    `
+<li>
+<a href="" class="smooth">
+    <i class="linecons-star"></i>
+    <span class="title">关注公众号</span>
+    <span class="picture"><img style="max-width:100%;margin-top:.8rem;" src="https://weqihang.cn/wp-content/uploads/2022/02/qrcode_wechat.jpg" alt="公众号"></span>
+</a>
+</li>
+`;
+// 2.回主站的链接
+
+const logo_link =
+    `
+<div class="logo">
+<a href="https://weqihang.cn/" class="logo-expanded">
+
+    <p class="overflowClip_2" style="font-size: 20px;"><strong>启航者外贸导航</strong></p>
+
+</a>
+<a href="https://weqihang.cn/" class="logo-collapsed">
+
+    <p class="overflowClip_2" style="font-size: 20px;"><strong>启航者外贸导航</strong></p>
+
+</a>
+</div>
+`;
