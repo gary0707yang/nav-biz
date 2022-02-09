5d222ddbcabfd119831d363060d77642313507d9
Gary Yang <feiyang@live.cn>
2022-02-09 11:00:17 +0800

    config change

--- update.js
+++ update.js
@@ -35,19 +35,42 @@ const two_search_engine =
 // wiki
 //quora
 
-
-{
-    "name": "维基百科",
-    "img": "url('https://cdn.jsdelivr.net/gh/x1ah/webstack-assets@master/assets/images/search_icon.png')  -105px 0px",
-    "position": "-40px 0px",
-    "url": "https://en.wikipedia.org/w/index.php?search="
-},
-{
-    "name": "Quora",
-    "img": "url('https://cdn.jsdelivr.net/gh/x1ah/webstack-assets@master/assets/images/search_icon.png')  -105px 0px",
-    "position": "-40px 0px",
-    "url": "https://www.quora.com/search?q="
-}
+var searchData = {
+    "thisSearch": "https://www.google.com/search?q=",
+    "thisSearchIcon": "https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png",
+    "hotStatus": true,
+    "data": [{
+        "name": "谷歌",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png')  -105px 0px",
+        "position": "-40px 0px",
+        "url": "https://www.google.com/search?q="
+    }, {
+        "name": "必应",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png')  -80px -25px",
+        "position": "0px -40px",
+        "url": "https://cn.bing.com/search?q="
+    }, {
+        "name": "百度",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png') -80px 0px",
+        "position": "0px 0px",
+        "url": "https://www.baidu.com/s?wd="
+    }, {
+        "name": "知乎",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png') -105px -100px",
+        "position": "-40px -160px",
+        "url": "https://www.zhihu.com/search?type=content\u0026q="
+    },{
+        "name": "维基百科",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png') -80px -200px",
+        "position": "0px -320px",
+        "url": "https://en.wikipedia.org/w/index.php?search="
+    },
+    {
+        "name": "Quora",
+        "img": "url('https://s3.bmp.ovh/imgs/2022/02/198b948ebd0c2e67.png')  -105px -175px",
+        "position": "-40px -280px",
+        "url": "https://www.quora.com/search?q="
+    }]
 
 
 `
