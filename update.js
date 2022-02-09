8c42769ee1b4144b7bed64170e61d8a3bbd5415b
Gary Yang <feiyang@live.cn>
2022-02-09 09:55:26 +0800

    add search engines

--- update.js
+++ update.js
@@ -17,13 +17,37 @@ const logo_link =
 <div class="logo">
 <a href="https://weqihang.cn/" class="logo-expanded">
 
-    <p class="overflowClip_2" style="font-size: 20px;"><strong>启航者外贸导航</strong></p>
+    <p class="overflowClip_2" style="font-size: 20px; color:white;"><strong>启航者</strong></p>
 
 </a>
 <a href="https://weqihang.cn/" class="logo-collapsed">
 
-    <p class="overflowClip_2" style="font-size: 20px;"><strong>启航者外贸导航</strong></p>
+    <p class="overflowClip_2" style="font-size: 20px; color:white;"><strong>启航者</strong></p>
 
 </a>
 </div>
 `;
+
+// 3. 搜索引擎
+
+const two_search_engine = 
+`
+// wiki
+//quora
+
+
+{
+    "name": "维基百科",
+    "img": "url('https://cdn.jsdelivr.net/gh/x1ah/webstack-assets@master/assets/images/search_icon.png')  -105px 0px",
+    "position": "-40px 0px",
+    "url": "https://en.wikipedia.org/w/index.php?search="
+},
+{
+    "name": "Quora",
+    "img": "url('https://cdn.jsdelivr.net/gh/x1ah/webstack-assets@master/assets/images/search_icon.png')  -105px 0px",
+    "position": "-40px 0px",
+    "url": "https://www.quora.com/search?q="
+}
+
+
+`
